578e12b52395421c1b6faadbe7b2840d189504b5
Troy Dai <git@troydai.com>
2026-01-17 23:12:22 -0800

    update

--- spire-agent-mock/src/main.rs
+++ spire-agent-mock/src/main.rs
@@ -1,23 +1,14 @@
+mod server;
+
 use anyhow::Result;
 use clap::Parser;
 use std::fs;
 use std::path::PathBuf;
-use std::pin::Pin;
 use tokio::net::UnixListener;
 use tokio_stream::wrappers::UnixListenerStream;
-use tokio_stream::Stream;
-use tonic::{transport::Server, Request, Response, Status};
+use tonic::transport::Server;
 
-pub mod workload {
-    tonic::include_proto!("_");
-}
-
-use workload::spiffe_workload_api_server::{SpiffeWorkloadApi, SpiffeWorkloadApiServer};
-use workload::{
-    JwtBundlesRequest, JwtBundlesResponse, JwtsvidRequest, JwtsvidResponse, ValidateJwtsvidRequest,
-    ValidateJwtsvidResponse, X509BundlesRequest, X509BundlesResponse, X509svidRequest,
-    X509svidResponse,
-};
+use server::{MockWorkloadApi, SpiffeWorkloadApiServer};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -30,59 +21,6 @@ struct Args {
         env = "SPIFFE_ENDPOINT_SOCKET"
     )]
     socket_path: PathBuf,
-}
-
-pub struct MyWorkloadApi {}
-
-#[tonic::async_trait]
-impl SpiffeWorkloadApi for MyWorkloadApi {
-    type FetchX509SVIDStream = Pin<Box<dyn Stream<Item = Result<X509svidResponse, Status>> + Send>>;
-
-    async fn fetch_x509svid(
-        &self,
-        _request: Request<X509svidRequest>,
-    ) -> Result<Response<Self::FetchX509SVIDStream>, Status> {
-        println!("Received FetchX509SVID request");
-        Err(Status::unimplemented("not implemented"))
-    }
-
-    type FetchX509BundlesStream =
-        Pin<Box<dyn Stream<Item = Result<X509BundlesResponse, Status>> + Send>>;
-
-    async fn fetch_x509_bundles(
-        &self,
-        _request: Request<X509BundlesRequest>,
-    ) -> Result<Response<Self::FetchX509BundlesStream>, Status> {
-        println!("Received FetchX509Bundles request");
-        Err(Status::unimplemented("not implemented"))
-    }
-
-    async fn fetch_jwtsvid(
-        &self,
-        _request: Request<JwtsvidRequest>,
-    ) -> Result<Response<JwtsvidResponse>, Status> {
-        println!("Received FetchJWTSVID request");
-        Err(Status::unimplemented("not implemented"))
-    }
-
-    type FetchJWTBundlesStream =
-        Pin<Box<dyn Stream<Item = Result<JwtBundlesResponse, Status>> + Send>>;
-
-    async fn fetch_jwt_bundles(
-        &self,
-        _request: Request<JwtBundlesRequest>,
-    ) -> Result<Response<Self::FetchJWTBundlesStream>, Status> {
-        println!("Received FetchJWTBundles request");
-        Err(Status::unimplemented("not implemented"))
-    }
-
-    async fn validate_jwtsvid(
-        &self,
-        _request: Request<ValidateJwtsvidRequest>,
-    ) -> Result<Response<ValidateJwtsvidResponse>, Status> {
-        println!("Received ValidateJWTSVID request");
-        Err(Status::unimplemented("not implemented"))
-    }
 }
 
 #[tokio::main]
@@ -109,7 +47,7 @@ async fn main() -> Result<()> {
     let uds = UnixListener::bind(&socket_path)?;
     let uds_stream = UnixListenerStream::new(uds);
 
-    let service = MyWorkloadApi {};
+    let service = MockWorkloadApi::new();
 
     Server::builder()
         .add_service(SpiffeWorkloadApiServer::new(service))
